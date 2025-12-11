4c1cc70561ec775c06736824fecb1c2ef7d07017
Syrov-Egor <syrov_ev@mail.ru>
2025-12-11 20:14:38 +0300

    Added unit cell data representation

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	x := []float64{298, 373, 473, 573, 673, 773, 873, 973, 1073, 1173, 1273}
-	y := []float64{7.749, 7.75, 7.755, 7.749, 7.767, 7.767, 7.775, 7.775, 7.782, 7.784, 7.792}
-	data, err := NewData2D(x, y)
+	ucd, err := CSVToUnitCell("Bi3NbTiO9.csv", Orthorhombic)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fitRes, err := PolyFit(*data, 2)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(fitRes)
+	fmt.Println(ucd)
 }
